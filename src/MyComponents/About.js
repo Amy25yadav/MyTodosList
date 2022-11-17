6f6152c630c7146b361dc6cdfb2658a0725276b3
Amit <yyadavamit25@gmail.com>
2022-11-17 12:32:37 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h2>what is a Todo list Application ? </h2>
+            <p>MyTodoList is designed to use as a riminder for a work or important event you have to do in a curtain time period, as you donnot want it to miss and regret later. So isS best to make  a riminder for every activity you do and utiize your time as best as possible.</p>
+        </div>
+    )
+}
